97c5c69879179256c17c547dfa9b3064ca70b4af
Merlin Unterfinger <info@munterfinger.ch>
2021-11-24 19:10:56 +0100

    Improve coverage of `dist_normalize`.

--- tests/testthat/test-dist.R
+++ tests/testthat/test-dist.R
@@ -1,3 +1,7 @@
 test_that("Distance normalization methods work", {
   expect_equal(dist_normalize(matrix(10), 10, "gaussian"), matrix(0.01))
+  expect_equal(dist_normalize(matrix(10), 10, "gravity"), matrix(0.01))
+  expect_equal(dist_normalize(matrix(10), 10, "exponential"), matrix(0.01))
+  expect_equal(dist_normalize(matrix(10), 10, "logistic"), matrix(2))
+  expect_error(dist_normalize(matrix(10), 10, "invalid_imp_function"), "Invalid `imp_function` value")
 })
